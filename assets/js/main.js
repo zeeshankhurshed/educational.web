a924032460eb551cd5d59fd5ebe7fb4ead9e3932
zeeshan <zeeshankhursheed680@gmail.com>
2023-06-22 15:49:28 +0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// let nav_list = document.querySelector("#nav-list");
+// let hamburger = document.querySelector(".hamburger");
+
+// hamburger.addEventListener('click',function(){
+//     nav_list.classList.toggle("nav-toggle");
+// })
